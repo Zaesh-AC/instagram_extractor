c174c0ca3c9fb2e774fb216aea75c317c7a76b80
Andrea Ciantelli <andrea.ciantelli@gmail.com>
2021-07-23 11:23:15 +0200

    class based update

--- instagram_data_extractor.py
+++ instagram_data_extractor.py
@@ -5,35 +5,39 @@ import pandas as pd
 
 MEDIA_DIR = 'media/'
 
-def get_dataframe():
-    df = pd.DataFrame()
-    for filename in os.listdir(MEDIA_DIR):
-        with open(f'{MEDIA_DIR+filename}') as f:
-            data = json.load(f)
-            df = pd.concat([df, pd.DataFrame(data["photos"])])
-    df["taken_at"] = pd.to_datetime(df["taken_at"])
-    df.drop(labels=["location"], axis=1, inplace=True)
-    return df
 
-def get_caption(group):
-    return group[0][0]
+class Extractor():
 
-def get_date(group):
-    return group[0][1]
+    def get_dataframe(self):
+        df = pd.DataFrame()
+        for filename in os.listdir(MEDIA_DIR):
+            with open(f'{MEDIA_DIR+filename}') as f:
+                data = json.load(f)
+                df = pd.concat([df, pd.DataFrame(data["photos"])])
+        df["taken_at"] = pd.to_datetime(df["taken_at"])
+        df.drop(labels=["location"], axis=1, inplace=True)
+        return df
 
-def get_paths(group):
-    return group[1]["path"]
+    def get_caption(self, group):
+        return group[0][0]
+
+    def get_date(self, group):
+        return group[0][1]
+
+    def get_paths(self, group):
+        return group[1]["path"]
 
 if __name__ == '__main__':
     if not sys.platform == 'linux':
         raise ('Sorry, this script will not work on your Operating System :( ')
-    df = get_dataframe()
+    extractor = Extractor()
+    df = extractor.get_dataframe()
     for group in df.groupby(["caption", "taken_at"], axis=0):
-        caption = get_caption(group)
-        taken_at = get_date(group)
+        caption = extractor.get_caption(group)
+        taken_at = extractor.get_date(group)
         subprocess.run(f'mkdir "output/{taken_at}"', shell=True)
         if caption != '':
             with open(f'output/{taken_at}/caption.txt', 'w') as file:
                 file.write(caption)
-        for path in get_paths(group):
+        for path in extractor.get_paths(group):
             subprocess.run(f'cp {path} "output/{taken_at}"', shell=True)
