453733275a3cd5b4ef9e4b05cde13cfc4991652f
Andrea Ciantelli <a.ciantelli@dunbar.it>
2021-07-27 10:38:08 +0200

    update after logic division

--- instagram_extractor_script.py
+++ instagram_extractor_script.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 
-import subprocess
+import subprocess, os
 from instagram_extractor import Extractor
+
+MEDIA_DIR = 'media/'
+
 
 if __name__ == '__main__':
     extractor = Extractor()
-    df = extractor.get_dataframe()
+    for filename in os.listdir(MEDIA_DIR):
+        file = f"{MEDIA_DIR+filename}"
+        df = extractor.get_dataframe(file)
     for group in df.groupby(["caption", "taken_at"], axis=0):
         caption = extractor.get_caption(group)
         taken_at = extractor.get_date(group)
