cc99af5368be5879fc3c7e8f445b6ac12bdcd6d5
Andrea Ciantelli <a.ciantelli@dunbar.it>
2021-07-27 10:38:38 +0200

    importing method for dataframe

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
+import json
 import logging
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from instagram_extractor import Extractor
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET", "POST"])
-def hello_world():
+def home():
+    if request.method == "POST":
+        extractor = Extractor
+        for file in request.files["file"]:
+            df = extractor.get_dataframe(file)
     return render_template("homepage.html")
