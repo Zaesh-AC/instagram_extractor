1c54477eda0b7e9124606590731c908093547f22
Andrea Ciantelli <a.ciantelli@dunbar.it>
2021-07-27 10:37:04 +0200

    make class importable

--- instagram_extractor.py
+++ instagram_extractor.py
@@ -4,20 +4,17 @@ import pandas as pd
 
 from json.decoder import JSONDecodeError
 
-MEDIA_DIR = 'media/'
-
 
 class Extractor():
 
-    def get_dataframe(self):
+    def get_dataframe(self, file):
         df = pd.DataFrame()
-        for filename in os.listdir(MEDIA_DIR):
-            with open(f'{MEDIA_DIR+filename}') as f:
-                try:
-                    data = json.load(f)
-                    df = pd.concat([df, pd.DataFrame(data["photos"])])
-                except JSONDecodeError:
-                    raise ValueError('JSON decode error')
+        with open(file) as f:
+            try:
+                data = json.load(f)
+                df = pd.concat([df, pd.DataFrame(data["photos"])])
+            except JSONDecodeError:
+                raise ValueError('JSON decode error')
         df["taken_at"] = pd.to_datetime(df["taken_at"])
         df.drop(labels=["location"], axis=1, inplace=True)
         return df
