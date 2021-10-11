de96902ea005f8cc432e73d360fd8e5a1bbebee9
Andrea Ciantelli <andrea.ciantelli@gmail.com>
2021-10-11 15:57:10 +0200

    caption file creation and zip preparation

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import logging
+from zipfile import ZipFile
 
 from plugins import extract_information
 from plugins.settings import OUTPUT_DIR
@@ -14,14 +15,29 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
+def get_image_path(post):
+    return post[0].split("/")
+
+def get_image_source(post):
+    return get_image_path(post)[:3]
+
+def get_image_filename(post):
+    return get_image_path(post)[3:4][0]
+
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    if request.method == "POST":
+    if request.method == "POST" and request.files["posts"]:
         file = request.files["posts"]
-        username = request.form.get("username")
         posts = extract_information(file)
-        for post in enumerate(posts):
-            if f"{OUTPUT_DIR}/{username}/":
-                pass
+        username = request.form.get('username')
+        with ZipFile(f"data/{username}.zip", mode="x") as zip:
+            for post in posts:
+                source = "/".join(get_image_source(post))
+                filename = get_image_filename(post)
+                file_path = f"data/{source}/{filename.split('.')[0]}"
+                with open(f"{file_path}.txt", 'w+') as caption:
+                    caption.write(post[1])
+                zip.write(f"{file_path}.jpg")
+                zip.write(f"{file_path}.txt")
     return render_template("homepage.html")
--- plugins/instagram_extractor.py
+++ plugins/instagram_extractor.py
@@ -9,13 +9,8 @@ logger = logging.getLogger(__name__)
 
 def extract_information(file):
     soup = BeautifulSoup(file, "html.parser")
-    post = []
     divs = soup.find_all("div", {"class" : POST_CSS_CLASS})
-    for div in divs:
-        image = div.find("img")["src"]
-        try:
-            caption = div.find("div", {"class": CAPTION_CSS_CLASS}).text
-        except Exception:
-            caption = ""
-        post.append((image, caption))
-    return post
+    return [
+        (div.find("img")["src"], div.find("div", {"class": CAPTION_CSS_CLASS}).text)
+        for div in divs
+    ]
