9175ed224a5e8e3cdab0206b1efc10845c3356e6
Andrea Ciantelli <andrea.ciantelli@gmail.com>
2021-10-12 09:23:48 +0200

    file upload and save

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import logging
+import os
 from zipfile import ZipFile
-
-from plugins import extract_information, get_file_path
-from plugins.settings import OUTPUT_DIR
 
 from flask import (
     Flask,
@@ -11,25 +9,33 @@ from flask import (
     send_file
 )
 
+from plugins import extract_information, get_file_path
+from plugins.settings import UPLOAD_FOLDER
+
+
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST" and request.files["posts"]:
         file = request.files["posts"]
-        posts = extract_information(file)
         username = "username" if request.form.get("username") == "" else request.form.get("username")
-        zipfile = f"data/{username}.zip"
-        with ZipFile(zipfile, mode="x") as zip:
+
+        filename = f"{username}.zip"
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"], f"{username}_raw_data"))
+
+        posts = extract_information(file)
+        with ZipFile(filename, mode="x") as zip:
             for post in posts:
                 file_path = get_file_path(post)
                 with open(f"{file_path}.txt", 'w+') as caption:
                     caption.write(post[1])
                 zip.write(f"{file_path}.jpg")
                 zip.write(f"{file_path}.txt")
-        return send_file(zipfile)
+        return send_file(filename)
     return render_template("homepage.html")
--- plugins/settings.py
+++ plugins/settings.py
@@ -1,3 +1,3 @@
 POST_CSS_CLASS = "pam _3-95 _2ph- _2lej uiBoxWhite noborder"
 CAPTION_CSS_CLASS = "_3-95 _2pim _2lek _2lel"
-OUTPUT_DIR = "/output"
+UPLOAD_FOLDER = "uploads/"
